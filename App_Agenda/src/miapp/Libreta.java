5a98821bc298355952af8282cdd5ee5ac26900fb
YasminLago <ylagorebollar@danielcastelao.org>
2016-01-31 18:00:25 +0100

    Update Libreta.java

--- App_Agenda/src/miapp/Libreta.java
+++ App_Agenda/src/miapp/Libreta.java
@@ -1,44 +1,61 @@
 package miapp;
 //Yasmin
+
 public class Libreta {
-   // private String dni;
-    int libre=cualLibre();
-    Contacto[]arrayContactos=new Contacto[3];
-    public Libreta(){   
+
+    private String nome, dni;
+    private int libre = cualLibre();
+    Contacto[] arrayContactos = new Contacto[3];
+
+    public Libreta() {
+        nome = dni = "";
     }
-    /*public Libreta(String aux_nome, String aux_dni) {
-        /*this.nome = aux_nome;
+
+    public Libreta(String aux_nome, String aux_dni) {
+        this.nome = aux_nome;
         this.dni = aux_dni;
-        
     }
-   /* public String getNome() {
+
+    public String getNome() {
         return nome;
     }
 
     public String getDni() {
-        return dni;*/
-      public int cualLibre(){
-        for(libre=0;libre<arrayContactos.length;libre++){
-            if(arrayContactos[libre].getNome().equals("")){
-            System.out.println("Hay hueco");
-        return libre;
-            }else if(libre==arrayContactos.length){  
-            }      
-        }return -1;  
-    }  
-    public void engadir(){
-        libre=cualLibre();
-       if (libre>0){
-           System.out.println("Introduce los datos del contacto: ");
-           arrayContactos[libre-1].toString();
-       }
+        return dni;
     }
-    public void listar(){
-        for(int i=0;i<arrayContactos.length;i++){
-            if(arrayContactos[i].toString().equals("")){
-                
-                
+
+    public void engadir() {
+        libre = cualLibre();
+        if (libre > 0) {
+            System.out.println("Introduzca nombre");
+            arrayContactos[libre].setNome(nome);
+        }
+    }
+
+    public int cualLibre() {
+        for (int libre = 0; libre < arrayContactos.length; libre++) {
+            if (arrayContactos[libre].getNome().equals("")) {
+                System.out.println("Hay hueco");
+                return libre;
+            } else if (libre == arrayContactos.length) {
             }
-           }
-       }
+        }
+        return -1;
+    }
+
+    public void listar() {
+
+    }
+
+    public void engadirGrupo() {
+
+    }
+
+    public void borrarGrupo() {
+
+    }
+
+    public void listarGrupo() {
+
+    }
 }
