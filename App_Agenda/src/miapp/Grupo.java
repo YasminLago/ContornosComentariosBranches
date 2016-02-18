0e8ceed21904d40e15fcff26d9ef50afb3db8c90
ylagorebollar <ylagorebollar@atanasoff03.danielcastelao.org>
2016-02-18 08:58:26 +0100

    Merge origin/master
    Conflicts:
    	App_Agenda/src/miapp/Contacto.java
    	App_Agenda/src/miapp/Libreta.java

--- App_Agenda/src/miapp/Grupo.java
+++ App_Agenda/src/miapp/Grupo.java
@@ -1,21 +1,46 @@
 package miapp;
-//Yasmin
+/**
+ * Modificacions nos grupos da libreta
+ * @author ylagorebollar
+* @version 2.0
+ */
 public class Grupo {
 String nome;
     Contacto[]contactos;
+
+    /**
+     *
+     */
     public Grupo() {
     }
 
+    /**
+     *
+     * @param nome
+     * @param contactos
+     */
     public Grupo(String nome, Contacto[] contactos) {
         this.nome = nome;
         this.contactos = contactos;
     }
+
+    /**
+     *Engade contactos o grupos
+     */
     public void engadirContactoGrupo(){
         
     }
+
+    /**
+     *Elimina contactos dos grupos
+     */
     public void borrarContactoGrupo(){
         
     }
+
+    /**
+     *
+     */
     public void listarContactos(){
         
     }
