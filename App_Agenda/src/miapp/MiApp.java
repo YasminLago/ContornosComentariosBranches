a26d93bf994a960797b42152f7194c84be8ae78d
ylagorebollar <ylagorebollar@atanasoff03.danielcastelao.org>
2016-02-18 08:58:23 +0100

    Merge origin/master
    Conflicts:
    	App_Agenda/src/miapp/Contacto.java
    	App_Agenda/src/miapp/Libreta.java

--- App_Agenda/src/miapp/MiApp.java
+++ App_Agenda/src/miapp/MiApp.java
@@ -1,6 +1,16 @@
 package miapp;
 //Yasmin
+
+/**
+ *
+ * @author ylagorebollar
+ */
 public class MiApp {
+
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
    
        }
